3544bfc0632e851e25645446b53154f64dab4e37
JuanK <juancamilo.manjarres@gmail.com>
2024-09-07 18:24:26 -0500

    feat: mainnet transactions

--- src/utils/avalanche.js
+++ src/utils/avalanche.js
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
 
-const AVALANCHE_RPC_URL = process.env.NEXT_PUBLIC_AVALANCHE_RPC_URL; // Replace with your AvaCloud RPC URL
+const AVALANCHE_RPC_URL = 'https://rpc.ankr.com/avalanche'; // Replace with your AvaCloud RPC URL
 const CHAIN_ID = 43114; // For Avalanche C-Chain Mainnet, change as needed for your L1
 
 const provider = new ethers.JsonRpcProvider(AVALANCHE_RPC_URL);
